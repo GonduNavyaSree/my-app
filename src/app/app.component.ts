6a94dc5476755875ddca87cde9b095ae65847cf5
GonduNavyaSree <gondunavyasrie@gmail.com>
2024-07-25 22:41:34 +0530

    user-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,45 +17,6 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  movie = [
-    {
-      id: "99",
-      name: "Vikram",
-      poster: "https://m.media-amazon.com/images/M/MV5BMmJhYTYxMGEtNjQ5NS00MWZiLWEwN2ItYjJmMWE2YTU1YWYxXkEyXkFqcGdeQXVyMTEzNzg0Mjkx._V1_.jpg",
-      rating: 8.4,
-      summary: "Members of a black ops team must track and eliminate a gang of masked murderers.",
-      trailer: "https://www.youtube.com/embed/OKBMCL-frPU"
-    },
-    {
-      id: "100",
-      name: "RRR",
-      poster: "https://englishtribuneimages.blob.core.windows.net/gallary-content/2021/6/Desk/2021_6$largeimg_977224513.JPG",
-      rating: 8.8,
-      summary: "RRR is an upcoming Indian Telugu-language period action drama film directed by S. S. Rajamouli, and produced by D. V. V. Danayya of DVV Entertainments.",
-      trailer: "https://www.youtube.com/embed/f_vbAtFSEc0"
-    },
-    {
-      id: "101",
-      name: "Iron man 2",
-      poster: "https://m.media-amazon.com/images/M/MV5BMTM0MDgwNjMyMl5BMl5BanBnXkFtZTcwNTg3NzAzMw@@._V1_FMjpg_UX1000_.jpg",
-      rating: 7,
-      summary: "With the world now aware that he is Iron Man, billionaire inventor Tony Stark (Robert Downey Jr.) faces pressure from all sides to share his technology with the military. He is reluctant to divulge the secrets of his armored suit, fearing the information will fall into the wrong hands. With Pepper Potts (Gwyneth Paltrow) and Rhodes (Don Cheadle) by his side, Tony must forge new alliances and confront a powerful new enemy.",
-      trailer: "https://www.youtube.com/embed/wKtcmiifycU"
-    },
-    {
-      id: "104",
-      name: "The Avengers",
-      rating: 8,
-      summary: "Marvel's The Avengers (classified under the name Marvel Avengers\n Assemble in the United Kingdom and Ireland), or simply The Avengers, is\n a 2012 American superhero film based on the Marvel Comics superhero team\n of the same name.",
-      poster: "https://terrigen-cdn-dev.marvel.com/content/prod/1x/avengersendgame_lob_crd_05.jpg",
-      trailer: "https://www.youtube.com/embed/eOrNdBpGMv8"
-    }, 
-  ]  
-
-  show = true;
-
-  displaytext() {
-    this.show = !this.show;
-  }
+ 
 }
 
