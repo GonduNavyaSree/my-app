5b86995697b6bf8f47ee882a097f48a29f49134e
GonduNavyaSree <gondunavyasrie@gmail.com>
2024-07-29 12:53:18 +0530

    Added Input fields to Movie List

--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -2,13 +2,26 @@ import { Component, Input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { IMovie } from '../app.component';
 import { MovieComponent } from '../movie/movie.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-movie-list',
   standalone: true,
-  imports: [FormsModule, MovieComponent],
+  imports: [
+    FormsModule,
+    MovieComponent,
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   templateUrl: './movie-list.component.html',
-  styleUrl: './movie-list.component.scss'
+  styleUrl: './movie-list.component.scss',
 })
 export class MovieListComponent {
   @Input() movies: Array<IMovie> = [
