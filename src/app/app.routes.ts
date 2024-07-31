847267be2f845d8f87f92b833b260b9bff9dcc1f
GonduNavyaSree <gondunavyasrie@gmail.com>
2024-07-31 15:31:43 +0530

    Auth Guard Route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { ColorGameComponent } from './color-game/color-game.component';
 import { UserAppComponent } from './user-app/user-app.component';
 import { MovieListComponent } from './movie-list/movie-list.component';
 import { MovieComponent } from './movie/movie.component';
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
+import { authGuard } from './auth.guard';
 
 export const routes: Routes = [
     {
@@ -11,14 +13,19 @@ export const routes: Routes = [
     },
     {
         path: 'color',
-        component: ColorGameComponent,
+        component: ColorGameComponent, canActivate: [authGuard]
     },
     {
         path: 'user',
         component: UserAppComponent,
     },
     {
-        path: 'movie/:id',
-        component: MovieComponent,
-    },
+        path: 'movies', // url
+        children: [
+          { path: '', component: MovieListComponent },
+          { path: 'movie', component: MovieComponent },
+          { path: ':id', component: MovieDetailsComponent },
+
+        ],
+    }
 ];
